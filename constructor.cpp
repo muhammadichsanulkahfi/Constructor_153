943743bba74bb18cd02ef95be9854cda2a37895c
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-05-13 18:37:19 +0700

    comit1

---
+++ constructor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa{
+public:
+mahasiswa(); // Constructor mahasiswa
+};
+
+mahasiswa::mahasiswa(){
+    cout<<"Constructor Terpanggil"<<endl; //Definisi Constructor
+}:
