c977c7d140af749ca51d7e75af3a98b48fdd4479
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-05-13 18:40:21 +0700

    comit2

--- constructor.cpp
+++ constructor.cpp
@@ -8,4 +8,9 @@ mahasiswa(); // Constructor mahasiswa
 
 mahasiswa::mahasiswa(){
     cout<<"Constructor Terpanggil"<<endl; //Definisi Constructor
-}:
+};
+
+int main(){
+    mahasiswa mhs; //Pembuatan Objeck
+    return 0;
+}
