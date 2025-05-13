3de2160f7c648e94b3bf29ee36e0a5e6f69e3c65
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-05-13 19:16:51 +0700

    comit7

--- constructoroverloding.cpp
+++ constructoroverloding.cpp
@@ -15,5 +15,31 @@ void cetak();
 };
 
 mahasiswa :: mahasiswa(){
-    
+}
+
+mahasiswa :: mahasiswa(int iNim){
+    nim = iNim; //definisi hanya NIM
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama; //definisi hanya NAMA
+}
+
+void mahasiswa::cetak(){
+
+    cout << endl << "Nim = " <<nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs1(102030);             //hanya NIM
+    mahasiswa mhs2("Andi");             //hanya NAMA
+    mahasiswa mhs3(010203, "Santi");    //NIM dan nama
+
+    //tampilkan nilai
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
 }
